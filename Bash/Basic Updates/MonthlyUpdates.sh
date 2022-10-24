4c70dd00cff7cf89ea14764e49a386c5ed286f0a
Steve Schulz <56364174+Steve-Schulz@users.noreply.github.com>
2022-10-24 16:13:53 -0400

    Update MonthlyUpdates.sh

--- Bash/Basic Updates/MonthlyUpdates.sh
+++ Bash/Basic Updates/MonthlyUpdates.sh
@@ -5,14 +5,24 @@ if [ "$EUID" -ne 0 ]
 fi
 echo "Auto-Updating"
 dateTime=$(date) 
-echo "Monthly Updates for $dateTime" > Updates.log
-echo >> Updates.log
-echo "Updating apt database" >> Updates.log
-apt-get update >> Updates.log
-echo >> Updates.log
-echo "Auto-removing old dependencies" >> Updates.log
-apt-get autoremove -y >> Updates.log
-echo >> Updates.log
-echo "Applying upgrades" >> Updates.log
-apt-get upgrade -y >> Updates.log
-reboot
+echo "Monthly Updates for $dateTime" > updates.log
+echo >> updates.log
+echo "Updating apt database" >> updates.log
+apt-get update >> updates.log
+echo >> updates.log
+echo "Auto-removing old dependencies" >> updates.log
+if [ "$1" = "--do-upgrade" ]
+    then 
+        apt-get autoremove -y >> updates.log
+    else
+        echo >> "--do-upgrade not passed" >> updates.log
+fi
+echo >> updates.log
+echo "Applying upgrades" >> updates.log
+if [ "$1" = "--do-upgrade" ]
+    then
+        apt-get upgrade -y >> updates.log
+        reboot
+    else
+        echo >> "--do-upgrade not passed" >> updates.log
+fi
